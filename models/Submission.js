3b57ecb7a8a178ca00eea1b1a17d2db27b1419ab
Ajayi Obaloluwa Emmanuel <obaloluwaajayi2006@gmail.com>
2026-06-01 12:58:09 -0700

    Update Submission.js

--- models/Submission.js
+++ models/Submission.js
@@ -1,51 +1,67 @@
 import mongoose from "mongoose";
 
-const submissionSchema = new mongoose.Schema({
-
-    student:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true
+const submissionSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
     },
 
-    assignment:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Assignment",
-        required:true
+    assignment: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Assignment",
+      required: true
     },
 
-    fileUrl:{
-        type:String,
-        required:true
+    fileUrl: {
+      type: String,
+      required: true
     },
 
-    grade:{
-        type:Number,
-        default:null
+    grade: {
+      type: Number,
+      default: null
     },
 
-    feedback:{
-        type:String,
-        default:""
+    feedback: {
+      type: String,
+      default: ""
     },
 
-    status:{
-        type:String,
-        enum:[
-            "pending",
-            "reviewed"
-        ],
-        default:"pending"
+    status: {
+      type: String,
+      enum: [
+        "pending",
+        "reviewed"
+      ],
+      default: "pending"
     }
+  },
+  {
+    timestamps: true
+  }
+);
 
-},
-{
-    timestamps:true
-});
+/*
+ Prevent duplicate submissions
+ One student can submit
+ one assignment only once
+*/
+
+submissionSchema.index(
+  {
+    student: 1,
+    assignment: 1
+  },
+  {
+    unique: true
+  }
+);
 
 const Submission = mongoose.model(
-    "Submission",
-    submissionSchema
+  "Submission",
+  submissionSchema
 );
 
 export default Submission;
